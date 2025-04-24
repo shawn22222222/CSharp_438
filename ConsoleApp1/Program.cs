993aa4a14a2b8fdd54cd49656261bc288633a245
shawn <shawn@LAPTOP-P7AICLEB>
2025-04-24 17:14:03 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region  計算圓面積
+            //int r = 10;
+            //double area = r * r * 3.14;
+            //Console.WriteLine(area);
+            #endregion
+
+            #region double計算
+            //double x = 1 / 2;  // 輸出0
+            //Console.WriteLine(x);
+            #endregion
+
+            #region 手動輸入圓半徑計算面積
+            //Console.WriteLine("輸入圓半徑");
+            //uint v;
+            //bool r = uint.TryParse(Console.ReadLine(), out v);
+            //double A = v * v * 3.14;
+            //Console.WriteLine(A);
+            #endregion
+
+            #region 產生兩個隨機數相加 (0~9)
+            Random rng = new Random();
+            int x = rng.Next(10);
+            int y = rng.Next(10);
+            Console.WriteLine("{0} + {1} = ?", x, y);
+
+            int num = int.Parse(Console.ReadLine());
+            if (num == x + y)
+            {
+                Console.WriteLine("correct");
+            }
+            else
+            {
+                Console.WriteLine("wrong. It is {0}", x+y);
+            }
+            #endregion
+
+        }
+    }
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("(0, 99)?");
+            int guess;
+            Random num = new Random();
+            int gameNum = num.Next(100);
+            int lowerNum = 0;
+            int upperNum = 99;
+
+
+            while (true)
+            {
+                guess = int.Parse(Console.ReadLine());
+                if(guess < 0)
+                {
+                    Console.WriteLine("Out of range. Try again?");
+                    continue;
+                }
+                if (guess < lowerNum || guess > upperNum)
+                {
+                    Console.WriteLine("Out of range. Try again?");
+                    continue;
+                }
+               
+                if (guess > gameNum)
+                {
+                    upperNum = guess - 1;
+                }
+                else if (guess < gameNum)
+                {
+                    lowerNum = guess + 1;
+                }
+                else
+                {
+                    Console.WriteLine("Bingo.");
+                    break;
+                }
+                if (lowerNum == upperNum)
+                {
+                    Console.WriteLine("GG.");
+                    break;
+                }
+                Console.WriteLine("({0},{1})", lowerNum, upperNum);
+
+            }
+
+        }
+    }
+}
+
