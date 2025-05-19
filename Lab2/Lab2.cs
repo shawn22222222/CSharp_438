97a2c9a20752ae76c1e68ba8c8657562777d39d9
shawn <shawn50110@gmail.com>
2025-05-19 19:39:13 +0800

    lab2

---
+++ Lab2/Lab2.cs
@@ -0,0 +1,43 @@
+﻿namespace Lab2
+{
+    internal class Lab2
+    {
+        static void Main(string[] args)
+        {
+            
+            int n = 16;
+            int[] arr = new int[n];
+            Console.Write("{0, 10}", "ID:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i;
+                Console.Write("{0, 5}", arr[i]);
+            }
+            Console.WriteLine();
+            Console.Write("{0, 10}", "Contactee:");
+            Random rng = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int j = rng.Next(i, n);
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                Console.Write("{0, 5}", arr[i]);
+            }
+            int target = 0;
+            int current = target;
+            Console.WriteLine();
+            do
+            {
+                current = arr[current];
+                Console.Write("{0, 3}", current);
+            }
+            while (current != target);
+
+        }
+    }
+}
+
+
+
+
